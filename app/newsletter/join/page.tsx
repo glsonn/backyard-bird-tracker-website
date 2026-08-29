bc23277db946c47c84283b801c9cabda4627ce44
Gary Sonnenberg <contact@parktracker.app>
2026-08-29 16:29:45 -0500

    Added newsletter join page

---
+++ app/newsletter/join/page.tsx
@@ -0,0 +1,23 @@
+import Container from "@/components/layout/Container";
+
+export default function NewsletterJoinPage() {
+  return (
+    <main className="newsletter-join-page">
+      <Container>
+        <section className="newsletter-join">
+          <h1>You&apos;re in! 🐦</h1>
+
+          <p>
+            <br />
+            You&apos;ll start receiving the Backyard Bird Tracker newsletter on
+            Friday mornings.
+            <br />
+            <br />
+          </p>
+
+          <p>Until then, look out the window. See who visits today.</p>
+        </section>
+      </Container>
+    </main>
+  );
+}
